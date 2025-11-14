ccaf74cdc75be6cfb3ae01fd98cb7862a30a2df0
Matthias Baer <matthias.r.baer@googlemail.com>
2025-11-14 11:47:32 +0100

    Setup using measure volt/frequency pairs

--- bmlab/models/setup.py
+++ bmlab/models/setup.py
@@ -176,6 +176,7 @@ class Calibration(Serializer):
         shift_methanol=None,
         shift_water=None,
         expected_shifts_polynomial=None,
+        expected_shifts_values=None,
     ):
         """
 
@@ -192,10 +193,19 @@ class Calibration(Serializer):
         self.num_brillouin_samples = num_brillouin_samples
         self.shift_methanol = shift_methanol
         self.shift_water = shift_water
+        self.expected_shifts_polynomial = None
+        self.expected_shifts_interpolator = None
 
         if expected_shifts_polynomial is not None:
             self.expected_shifts_polynomial = np.poly1d(
                 np.array(expected_shifts_polynomial[::-1])
+            )
+        elif expected_shifts_values is not None:
+            # Create linear interpolator from (voltage, frequency) tuples
+            voltages = np.array([v for v, f in expected_shifts_values])
+            frequencies = np.array([f for v, f in expected_shifts_values])
+            self.expected_shifts_interpolator = interpolate.interp1d(
+                voltages, frequencies, kind="linear", fill_value="extrapolate"
             )
         else:
             self.expected_shifts_polynomial = None
@@ -219,15 +229,12 @@ class Calibration(Serializer):
     def shifts(self):
         if self.expected_shifts_polynomial:
             return self.expected_shifts_polynomial
+        elif self.expected_shifts_interpolator is not None:
+            return self.expected_shifts_interpolator
         return self._shifts
 
     def update_calibration(self):
         # Construct array with the frequency shifts
-        if self.shift_methanol is None and self.shift_water is None:
-            if self.expected_shifts_polynomial is None:
-                raise ValueError(
-                    "Either provide expected shifts polynomial coefs or shift values."
-                )
 
         self._shifts = np.array([])
         if self.shift_methanol or self.shift_water:
@@ -371,6 +378,128 @@ AVAILABLE_SETUPS = [
                 0.000894938,
                 -0.0000432597,
                 7.69557e-7,
+            ],
+        ),
+        temperature=295.15,
+        extraction_method=ExtractionMethod.ARC_FROM_PTS_OF_ALL_IMGS,
+    ),
+    Setup(
+        key="S6",
+        name="MPZPM FOB, No Fit + Extrapolation",
+        pixel_size=6.5e-6,
+        focal_length=0.2,
+        vipa=VIPA(
+            d=0.006743,
+            n=1.45367,
+            theta=0.8 * 2 * np.pi / 360,
+            order=0,
+            lambda0=780.24e-9,
+        ),
+        calibration=Calibration(
+            num_brillouin_samples=1,
+            # coefficients for polynomial volt => frequency shift [Hz], lowest order first
+            expected_shifts_values=[
+                (0, 3.669157366),
+                (0.1, 3.702491629),
+                (0.2, 3.735825893),
+                (0.3, 3.769160156),
+                (0.4, 3.80249442),
+                (0.5, 3.835828683),
+                (0.6, 3.869162946),
+                (0.7, 3.90249721),
+                (0.8, 3.935831473),
+                (0.9, 3.969165737),
+                (1, 4.0025),
+                (1.1, 4.035834263),
+                (1.2, 4.069168527),
+                (1.3, 4.10250279),
+                (1.4, 4.135837054),
+                (1.5, 4.169171317),
+                (1.6, 4.20250558),
+                (1.7, 4.235839844),
+                (1.8, 4.269174107),
+                (1.9, 4.302508371),
+                (2, 4.335842634),
+                (2.1, 4.369176897),
+                (2.2, 4.402511161),
+                (2.3, 4.435845424),
+                (2.4, 4.469179688),
+                (2.5, 4.502513951),
+                (2.6, 4.535848214),
+                (2.7, 4.569182478),
+                (2.8, 4.602516741),
+                (2.9, 4.635851004),
+                (3, 4.669185268),
+                (3.1, 4.702519531),
+                (3.2, 4.735853795),
+                (3.3, 4.769188058),
+                (3.4, 4.802522321),
+                (3.5, 4.835856585),
+                (3.6, 4.869190848),
+                (3.7, 4.902525112),
+                (3.8, 4.935859375),
+                (3.9, 4.968984375),
+                (4, 5.0021875),
+                (4.1, 5.035),
+                (4.2, 5.068125),
+                (4.3, 5.101015625),
+                (4.4, 5.133828125),
+                (4.5, 5.1665625),
+                (4.6, 5.199375),
+                (4.7, 5.231875),
+                (4.8, 5.26453125),
+                (4.9, 5.29703125),
+                (5, 5.329296875),
+                (5.1, 5.361640625),
+                (5.2, 5.3940625),
+                (5.3, 5.42625),
+                (5.4, 5.458203125),
+                (5.5, 5.490390625),
+                (5.6, 5.5221875),
+                (5.7, 5.5540625),
+                (5.8, 5.585859375),
+                (5.9, 5.6175),
+                (6, 5.648984375),
+                (6.1, 5.680546875),
+                (6.2, 5.71203125),
+                (6.3, 5.743125),
+                (6.4, 5.774453125),
+                (6.5, 5.805625),
+                (6.6, 5.8365625),
+                (6.7, 5.867421875),
+                (6.8, 5.898359375),
+                (6.9, 5.9290625),
+                (7, 5.95953125),
+                (7.1, 5.990078125),
+                (7.2, 6.0203125),
+                (7.3, 6.050546875),
+                (7.4, 6.080703125),
+                (7.5, 6.110859375),
+                (7.6, 6.140625),
+                (7.7, 6.170390625),
+                (7.8, 6.19984375),
+                (7.9, 6.229375),
+                (8, 6.258828125),
+                (8.1, 6.288359375),
+                (8.2, 6.3178125),
+                (8.3, 6.346953125),
+                (8.4, 6.37609375),
+                (8.5, 6.405078125),
+                (8.6, 6.43390625),
+                (8.7, 6.46265625),
+                (8.8, 6.491328125),
+                (8.9, 6.51984375),
+                (9, 6.548203125),
+                (9.1, 6.576484375),
+                (9.2, 6.604609375),
+                (9.3, 6.632734375),
+                (9.4, 6.660703125),
+                (9.5, 6.688828125),
+                (9.6, 6.716640625),
+                (9.7, 6.74421875),
+                (9.8, 6.7715625),
+                (9.9, 6.798828125),
+                (10, 6.825859375),
             ],
         ),
         temperature=295.15,
